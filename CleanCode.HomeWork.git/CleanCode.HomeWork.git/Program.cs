55c50273014abf40e7c247d1f24a8db69628af8c
denoolen <87325015+denoolen@users.noreply.github.com>
2022-02-20 22:36:18 +0200

    вроде дз

--- CleanCode.HomeWork.git/CleanCode.HomeWork.git/Program.cs
+++ CleanCode.HomeWork.git/CleanCode.HomeWork.git/Program.cs
@@ -1,18 +1,34 @@
-﻿Console.WriteLine("Давай давай сыграем в игру /" +
-                  "Введите число от 1 до 99");
-
-int secretrNumber = 10;
-
-    int userNumber;
-    userNumber = Convert.ToInt32(Console.ReadLine());
-    if (userNumber <= 0 | userNumber >= 100) 
-
+﻿Console.WriteLine("Давай давай сыграем в игру ");
+Console.WriteLine("Введи число 300 ( шутка про тракториста ) что-бы остановить игру");
+bool win = false;
+do
+{
+    int stopGame = 300;
+    int secretrNumber = 10;
+    Console.WriteLine("Введите число от 1 до 99");   
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber <= 0 && userNumber >= 100)
         Console.WriteLine("Введи верное число");
-    else
-        Console.WriteLine("не угадал!");
+    else if(userNumber > secretrNumber  )
+        Console.WriteLine($"не угадал! {userNumber} больше чем нужно");
+    else if (userNumber < secretrNumber)
+        Console.WriteLine($"не угадал! {userNumber} меньше чем нужно");
+
+    
+    if (userNumber == stopGame)
+    {
+        Console.WriteLine("Вы остановили игру");
+        break;
+    }
 
     if (userNumber == secretrNumber)
-
+    {
+        win = true;
         Console.WriteLine("Ты выиграл");
+    }
+  
+}
+while (!win);
+     
 
  
