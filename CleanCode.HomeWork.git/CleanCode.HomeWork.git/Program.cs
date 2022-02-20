d583ac08d3257bfa0b8871adcfa88e2df23300ba
denoolen <87325015+denoolen@users.noreply.github.com>
2022-02-20 19:22:12 +0200

    add home work

---
+++ CleanCode.HomeWork.git/CleanCode.HomeWork.git/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Давай давай сыграем в игру /" +
+                  "Введите число от 1 до 99");
+
+int secretrNumber = 10;
+
+    int userNumber;
+    userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber <= 0 | userNumber >= 100) 
+
+        Console.WriteLine("Введи верное число");
+    else
+        Console.WriteLine("не угадал!");
+
+    if (userNumber == secretrNumber)
+
+        Console.WriteLine("Ты выиграл");
+
+ 
