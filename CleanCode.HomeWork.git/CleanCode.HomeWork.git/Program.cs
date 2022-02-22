1f493aa9533484b0ca640dc12389b7ac1b63d87f
denoolen <87325015+denoolen@users.noreply.github.com>
2022-02-22 16:46:41 +0200

    fix known issues

--- CleanCode.HomeWork.git/CleanCode.HomeWork.git/Program.cs
+++ CleanCode.HomeWork.git/CleanCode.HomeWork.git/Program.cs
@@ -1,32 +1,40 @@
 ﻿Console.WriteLine("Давай давай сыграем в игру ");
 Console.WriteLine("Введи число 300 ( шутка про тракториста ) что-бы остановить игру");
+int stopGame = 300;
+Random random = new();
+int secretNumber = random.Next(0, 99);
 bool win = false;
 do
 {
-    int stopGame = 300;
-    int secretrNumber = 10;
-    Console.WriteLine("Введите число от 1 до 99");   
-    int userNumber = Convert.ToInt32(Console.ReadLine());
-    if (userNumber <= 0 && userNumber >= 100)
-        Console.WriteLine("Введи верное число");
-    else if(userNumber > secretrNumber  )
-        Console.WriteLine($"не угадал! {userNumber} больше чем нужно");
-    else if (userNumber < secretrNumber)
-        Console.WriteLine($"не угадал! {userNumber} меньше чем нужно");
+    Console.WriteLine("Введите число от 1 до 99");
+    try {
+        
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+        
+        if (userNumber < 0 || userNumber > 100)
+        {
+            Console.WriteLine("Введи верное число");
+        }
+        else if (userNumber > secretNumber)
+            Console.WriteLine($"не угадал! {userNumber} больше чем нужно");
+        else if (userNumber < secretNumber)
+            Console.WriteLine($"не угадал! {userNumber} меньше чем нужно");
 
-    
-    if (userNumber == stopGame)
-    {
-        Console.WriteLine("Вы остановили игру");
-        break;
-    }
 
-    if (userNumber == secretrNumber)
-    {
-        win = true;
-        Console.WriteLine("Ты выиграл");
+        if (userNumber == stopGame)
+        {
+            Console.WriteLine("Вы остановили игру");
+            break;
+        }
+
+        if (userNumber == secretNumber)
+        {
+            win = true;
+            Console.WriteLine("Ты выиграл");
+        }
     }
-  
+    catch { Console.WriteLine("введи число, але"); }
+   
 }
 while (!win);
      
